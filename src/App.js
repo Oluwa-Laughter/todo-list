d498cc8ef4588d589063236cd1d48b67c4efe8d6
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-09-19 04:10:49 +0100

    update todo UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Form onAddTodos={handleAddTodo} />
+      <TodoList todos={todos} />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <h1>Todo App</h1>;
+}
+
+function Form({ onAddTodos }) {
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+
+    const newTodo = {
+      text,
+      id: Date.now(),
+      completed: false,
+    };
+    onAddTodos(newTodo);
+
+    setText("");
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form-data">
+      <label htmlFor="todo">What needs to be done ?</label>
+      <input
+        type="text"
+        value={text}
+        id="todo"
+        name="text"
+        autoComplete="off"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit" className="btn">
+        Add
+      </button>
+    </form>
+  );
+}
+
+function TodoList({ todos }) {
+  const [sortBy, setSortBy] = useState("all");
+  return (
+    <div className="todo-list">
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="all">All</option>
+          <option value="active">Active</option>
+          <option value="completed">Completed</option>
+        </select>
+      </div>
+
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem todos={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function TodoItem({ todos }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span>{todos.text}</span>
+      <button>❌</button>
+    </li>
+  );
+}
